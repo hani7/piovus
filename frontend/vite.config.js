7f2a39d5ff56d00f55adaf384a4c5c6cc661e76e
hani7 <h.informatique7@gmail.com>
2026-06-04 15:59:19 +0100

    many

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,42 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import obfuscator from 'vite-plugin-javascript-obfuscator'
+
 
 export default defineConfig({
   plugins: [
-    react(),
-    obfuscator({
-      include: ['src/**/*.js', 'src/**/*.jsx'],
-      exclude: [/node_modules/],
-      apply: 'build',
-      debugger: true,
-      options: {
-        compact: true,
-        controlFlowFlattening: false,
-        deadCodeInjection: false,
-        debugProtection: false,
-        disableConsoleOutput: false,
-        identifierNamesGenerator: 'hexadecimal',
-        log: false,
-        numbersToExpressions: false,
-        renameGlobals: false,
-        selfDefending: false,
-        simplify: true,
-        splitStrings: false,
-        stringArray: true,
-        stringArrayCallsTransform: false,
-        stringArrayEncoding: [],
-        stringArrayIndexShift: true,
-        stringArrayRotate: true,
-        stringArrayShuffle: true,
-        stringArrayWrappersCount: 1,
-        stringArrayWrappersChainedCalls: true,
-        stringArrayWrappersParametersMaxCount: 2,
-        stringArrayWrappersType: 'variable',
-        stringArrayThreshold: 0.75,
-        unicodeEscapeSequence: false
-      }
-    })
+    react()
   ],
   server: {
     port: 5173,

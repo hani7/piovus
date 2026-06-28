e0b87a45df74ea0dfc6918cf09063bd6f3fdf864
hani7 <h.informatique7@gmail.com>
2026-06-28 15:42:35 +0100

    feat: add api/run-migrations route to trigger migrations via HTTP

--- pioveecom/urls.py
+++ pioveecom/urls.py
@@ -3,8 +3,22 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.http import HttpResponse
+
+def run_migration_view(request):
+    try:
+        from django.core.management import call_command
+        import io
+        out = io.StringIO()
+        call_command('migrate', interactive=False, stdout=out)
+        return HttpResponse(f"Migration SUCCESSFUL!<br><pre>{out.getvalue()}</pre>")
+    except Exception as e:
+        import traceback
+        return HttpResponse(f"Migration FAILED:<br><pre>{traceback.format_exc()}</pre>")
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/run-migrations/', run_migration_view),
     path('api/', include('pioveapp.urls')),
 ]
 

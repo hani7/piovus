afb99aac84800534b91bf90a5b1eed3db761eb7c
hani7 <h.informatique7@gmail.com>
2026-06-28 15:37:28 +0100

    feat: auto run migrations in passenger_wsgi for cPanel

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -10,4 +10,15 @@ sys.path.insert(0, os.path.dirname(__file__))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pioveecom.settings")
 
 from django.core.wsgi import get_wsgi_application
+
+# --- AUTOMATIC MIGRATION HACK FOR CPANEL ---
+try:
+    import django
+    django.setup()
+    from django.core.management import call_command
+    call_command('migrate', interactive=False)
+except Exception as e:
+    print(f"Migration error: {e}")
+# -------------------------------------------
+
 application = get_wsgi_application()

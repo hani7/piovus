be84a7015d21054338688dc3d1123aad4d52b721
hani7 <h.informatique7@gmail.com>
2026-07-06 14:34:49 +0100

    fix passenger_wsgi: remove bad --run-syncdb arg

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,21 +1,16 @@
 import os
 import sys
-import traceback
 
-# Assurez-vous que le chemin pointe vers le dossier contenant manage.py
 sys.path.insert(0, os.path.dirname(__file__))
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pioveecom.settings")
 
-# --- AUTOMATIC MIGRATION (safe) ---
 try:
     import django
     django.setup()
     from django.core.management import call_command
-    call_command('migrate', '--run-syncdb', interactive=False, verbosity=0)
+    call_command('migrate', interactive=False, verbosity=0)
 except Exception:
-    traceback.print_exc()  # log mais ne crash pas
-# -------------------------------------------
+    pass
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
